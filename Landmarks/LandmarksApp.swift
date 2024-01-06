d5a38aad601bea8819ee4ad45b67aaf6c86f5ec8
Hakan Sait Bozkurt <b.hakan@palladium.gen.tr>
2024-01-07 02:06:46 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hakan Sait Bozkurt on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
